964b959d1c44bfb67251e4717a374159620b34cd
aman-chaudhary-20110012 <aman.chaudhary@iitgn.ac.in>
2022-06-13 20:53:00 +0530

    updated

---
+++ main.js
@@ -0,0 +1,25 @@
+var map = L.map('map').setView([22.9074872, 79.07306671], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
